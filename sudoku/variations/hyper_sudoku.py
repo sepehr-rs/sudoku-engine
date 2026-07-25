505607272565f01488fac7f3d3af73b0f7679c6c
Sepehr Rasouli <sepehrrasouli06@gmail.com>
2026-07-25 09:57:26 +0330

    Add hyper sudoku

---
+++ sudoku/variations/hyper_sudoku.py
@@ -0,0 +1,28 @@
+# diagonal_sudoku.py
+
+from typing import List, Set
+from sudoku.base_sudoku import Pos
+from .classic_sudoku import ClassicSudoku
+
+
+class HyperSudoku(ClassicSudoku):
+    """
+    Standard NxN Sudoku with sub-boxes of size box_height x box_width.
+    Only rows, columns, and boxes are enforced.
+    """
+
+    def regions(self) -> List[Set[Pos]]:
+        """Return all standard Sudoku regions plus Hyper Sudoku boxes"""
+        regions = super().regions()
+        N = self.size
+
+        # Hyper Sudoku Boxes
+        for br in range(1, N, self.box_height + 1):
+            for bc in range(1, N, self.box_width + 1):
+                box = set()
+                for dr in range(3):
+                    for dc in range(3):
+                        box.add((br + dr, bc + dc))
+                regions.append(box)
+
+        return regions
